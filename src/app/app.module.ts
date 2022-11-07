ede9ed843c1f282c021c5a709a7584ce52cbdf82
rahul ramagiri <rahulramagiri8@gmail.com>
2022-11-07 11:48:52 +0530

    added routes and material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
-
+import { AppRoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './c/sidebar/sidebar.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent, SidebarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
